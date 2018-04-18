b0d5e7ae1bd3b5f974da59d79ed177e9f4492e82
Peter Schöppl <peterschoppl@peters-macbook-pro.local>
2018-04-18 10:50:18 +0200

    Initial Commit

---
+++ FSST_Project_2k18Tests/FSST_Project_2k18Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FSST_Project_2k18Tests.swift
+//  FSST_Project_2k18Tests
+//
+//  Created by Peter Schöppl on 18.04.18.
+//  Copyright © 2018 Peter Schöppl. All rights reserved.
+//
+
+import XCTest
+@testable import FSST_Project_2k18
+
+class FSST_Project_2k18Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
